980cf596eebedb525cd3b68792d7637afbcd8901
ChoiSangwon <fdsa200@naver.com>
2024-02-15 15:22:02 +0900

    feat : 20310.py

---
+++ baekjoon/20310.py
@@ -0,0 +1,26 @@
+arr = input()
+one_len = arr.count('1')
+zero_len = len(arr)-one_len
+res=arr
+count=0
+i=0
+while True:
+    if(count==one_len//2):
+        break
+    if(res[i]=='1'):
+        res = res[:i]+res[i+1:]
+        count+=1
+        continue
+    i+=1
+i=len(res)-1
+count=0
+while True:
+    if(count==zero_len//2):
+        break
+    if(res[i]=='0'):
+        res = res[:i]+res[i+1:]
+        count+=1
+        i-=1
+        continue
+    i-=1
+print(res)
